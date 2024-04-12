8ee8dca61a162025b3f1de07f9d16c75a161de5e
pspiagicw <pspiagicw@gmail.com>
2024-04-12 12:16:13 +0000

    feat: ReadFile read's a given file

--- fs.go
+++ fs.go
@@ -15,11 +15,11 @@ import (
 	"github.com/pspiagicw/goreland"
 )
 
-// Ensure given path exists on the file-system
-// - Creates directory if it doesn't exist
-// - Expects absolute path.
-// - Can't resolve home directories.
-// - Creates all parent directories if needed
+// Ensure given path exists on the file-system.
+//   - It creates directory if it doesn't exist.
+//   - It expects absolute path.
+//   - It can't resolve home directories.
+//   - It creates all parent directories if needed.
 func EnsurePathExists(location string) {
 	if !dirExists(location) {
 		err := os.MkdirAll(location, 0755)
@@ -28,6 +28,21 @@ func EnsurePathExists(location string) {
 		}
 	}
 }
+
+// Read file contents from the given ReadFile
+//   - Expects absolute path.
+//   - Can't resolve home directories.
+//   - Returns file contents as byte-array
+//   - Terminates execution if file can't be read
+func ReadFile(file string) []byte {
+	contents, err := os.ReadFile(file)
+	if err != nil {
+		goreland.LogFatal("Error reading file: %s, %v", file, err)
+	}
+
+	return contents
+}
+
 func dirExists(dir string) bool {
 	_, err := os.Stat(dir)
 	if errors.Is(err, fs.ErrNotExist) {
